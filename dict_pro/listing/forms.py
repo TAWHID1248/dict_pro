f1756db9982bc420f6931fdb0fc44e50d8b39a50
tawhid1248 <frzfardin2020@gmail.com>
2023-10-12 16:36:09 +0600

    form-widgets

--- dict_pro/listing/forms.py
+++ dict_pro/listing/forms.py
@@ -1,7 +1,22 @@
 from django import forms
 from .models import Post, Category
 
+choices = Category.objects.all().values_list('name', 'name')
+
+choice_list = []
+
+for item in choices:
+    choice_list.append(item)
+
+
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'category']
+
+
+        widgets = {
+                'title' : forms.TextInput(attrs={'class': 'form-control'}),
+                'content' : forms.Textarea(attrs={'class': 'form-control'}),
+                'category' : forms.Select(choices = choice_list, attrs={'class': 'form-control'}),
+            }
